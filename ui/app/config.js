fda124220a726c4cbb3750d4a4de508c76728966
federico-barreraoro <federico.barreraoro@mulesoft.com>
2018-02-23 22:10:21 -0300

    Removing unnecessary files

--- ui/app/config.js
+++
@@ -1,9 +0,0 @@
-angular.module('app.config', [])
-  .constant('configuration', {
-    host: 'http://localhost:8080/turns/',
-    securityGroupProtocols: 'tcp,udp,icmp',
-    version: '0.0.1',
-    lastUpdate: '2016-05-04',
-    trackingId: 'UA-76346601-4',
-    captchaSiteKey: '6LeSYB8TAAAAAEUCuC0wT-O2seZ0SdhyB5uK7d8t'
-  });
